f588f2d81419879f54e83db7c51ddf39526b499b
santhosh <skreddy753@gmail.com>
2025-11-09 17:25:21 +0530

    -- initial commit

--- programs/2_sample.py
+++ programs/2_sample.py
@@ -1,10 +1,61 @@
-import os, sys
+import os
+import sys
+
 from pyspark.sql import SparkSession
 
 os.environ['PYSPARK_PYTHON'] = sys.executable
 os.environ['JAVA_HOME'] = r'C:\Users\santh\.jdks\corretto-1.8.0_462'
 
+
 spark = SparkSession.builder.getOrCreate()
+sc= spark.sparkContext
 ###########################################
 
-spark.range(10).show()
+#spark.range(10).show()
+
+# print(" ==STARTED== ")
+#
+# a = 2
+# print(a)
+#
+# b = a + 2
+# print(b)
+#
+# c = "Zeyobron"
+# print(c)
+#
+# d = c + " Analytics"
+# print(d)
+
+
+
+
+
+
+
+
+
+
+
+print("=====started====")
+
+ls  =  [1,2,3,4]
+print()
+print("======rawList======")
+print(ls)
+
+rddls = sc.parallelize(ls)
+print()
+print("=====rddls======")
+print(rddls.collect())
+
+addrdd=rddls.map(  lambda x    :   x   +   2   )
+print()
+print("====addrdd======")
+print(addrdd.collect())
+
+
+
+
+
+
